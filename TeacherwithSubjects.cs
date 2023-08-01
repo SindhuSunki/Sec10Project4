6d11d82cb90efd0860acd8eab26f5584a50c79ef
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-08-01 18:38:56 +0530

    first commit

---
+++ TeacherwithSubjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    public class TeacherWithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherWithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+
+}
